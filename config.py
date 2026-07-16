a888f6e18d4cb233bc6c48abec3a56d7d37703f5
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-16 21:19:27 +0500

    update config.py, network settings written.

--- config.py
+++ config.py
@@ -2,3 +2,8 @@
 train_data = "data/brain-tumor-mri-deduplicated/data/train"
 test_data = "data/brain-tumor-mri-deduplicated/data/test"
 val_data = "data/brain-tumor-mri-deduplicated/data/val"
+
+# network settings
+img_size = 128
+batch_size = 32
+num_classes = 4
