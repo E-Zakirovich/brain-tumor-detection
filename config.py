3c9cb89330b1714d7a6d978483842a9ff7742892
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-16 21:18:03 +0500

    update config.py, dataset path stored inside

---
+++ config.py
@@ -0,0 +1,4 @@
+# dataset paths
+train_data = "data/brain-tumor-mri-deduplicated/data/train"
+test_data = "data/brain-tumor-mri-deduplicated/data/test"
+val_data = "data/brain-tumor-mri-deduplicated/data/val"
