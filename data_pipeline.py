e34088cb908b25116d429b8282fd13f65e4131dd
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-17 23:05:30 +0500

    update data_pipeline.py, I am working on to load training dataset. While I am training my network, I need to prevent my model from overfitting or underfitting, thats why train dataset must be sth different that usual.

--- data_pipeline.py
+++ data_pipeline.py
@@ -5,4 +5,28 @@ import config
 
 
 class DataPipeline:
-    ...
+
+    def __init__(self):
+
+        self.train_transform = transforms.Compose([
+            # changing the size of the images. The size of some images might not be same as others, so I need all images must be in same size
+            transforms.Resize(
+                (config.img_size, config.img_size)
+            ),
+
+            # to avoid overfitting, I want to flip half of train dataset
+            transforms.RandomHorizontalFlip(p=0.5),
+
+            # to avoid overfitting, I want to rotate images between -10 and 10 degree randomly
+            transforms.RandomResizedCrop(degrees=10),
+
+            # to learn my cnn, my dataset must be in tensors format not like a image or sth
+            transforms.ToTensor(),
+
+            # normalization part
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )
+
+        ])
